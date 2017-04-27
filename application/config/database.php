ec6d2a35c8a95fcfd02806337545315e534a1aad
SpriteN <MacBook_Pro_Rretina_512Gb@MacBooks-MacBook-Pro.local>
2017-04-27 09:54:09 +0300

    sutvarkytos klaidos

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost:8889',
 	'username' => '',
 	'password' => '',
 	'database' => '',
